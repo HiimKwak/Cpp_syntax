e7a941b41b2622ad70e778513649f7bdb9e01996
곽민규 <mindol0320@naver.com>
2026-01-30 10:01:24 +0900

    day12: functor

--- FunctionPointer/Main.cpp
+++ FunctionPointer/Main.cpp
@@ -1,46 +1,93 @@
+//#include <iostream>
+//#include <functional>
+//
+////typedef int (*Add)(int, int);
+//using Adder = int (*)(int a, int b);
+////using Adder = std::function<int(int, int)>;
+//using ClassAdder = int (Operator::*)(int a, int b);
+//
+//int A(int a, int b, Adder adder)
+////int A(int a, int b, std::function<int(int, int)> adder)
+//{
+//	return adder(a, b);
+//}
+//
+//int B(int a, int b, ClassAdder adder)
+////int A(int a, int b, std::function<int(int, int)> adder)
+//{
+//	return adder(a, b);
+//}
+//
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//class Operator
+//{
+//public:
+//	int Add(int a, int b)
+//	{
+//		return a + b;
+//	}
+//};
+//
+//int main()
+//{
+//	//int (*FunctionPointer)();
+//	//FunctionPointer = A;
+//
+//	Operator oper;
+//	//ClassAdder adder = &Operator::Add;
+//	//std::cout << A(10, 20, Add) << '\n'; // Operator::Add는 안됨. 함수포인터는 일반함수를 위한 문법
+//	std::cout << B(10, 20, Add);
+//
+//	std::cin.get();
+//}
+
 #include <iostream>
-#include <functional>
+#include <vector>
+#include <algorithm>
 
-//typedef int (*Add)(int, int);
-using Adder = int (*)(int a, int b);
-//using Adder = std::function<int(int, int)>;
-using ClassAdder = int (Operator::*)(int a, int b);
-
-int A(int a, int b, Adder adder)
-//int A(int a, int b, std::function<int(int, int)> adder)
-{
-	return adder(a, b);
-}
-
-int B(int a, int b, ClassAdder adder)
-//int A(int a, int b, std::function<int(int, int)> adder)
-{
-	return adder(a, b);
-}
-
-int Add(int a, int b)
-{
-	return a + b;
-}
-
-class Operator
+class Less
 {
 public:
-	int Add(int a, int b)
+	bool operator()(int a, int b) // 호출 연산자 오버로딩
 	{
-		return a + b;
+		return a < b;
 	}
+};
+
+class Greater
+{
+public:
+	bool operator()(int a, int b) // 호출 연산자 오버로딩
+	{
+		return a > b;
+	}
+private:
+	// 상태도 추가할 수 있음.
 };
 
 int main()
 {
-	//int (*FunctionPointer)();
-	//FunctionPointer = A;
+	std::vector<int> array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	Operator oper;
-	//ClassAdder adder = &Operator::Add;
-	//std::cout << A(10, 20, Add) << '\n'; // Operator::Add는 안됨. 함수포인터는 일반함수를 위한 문법
-	std::cout << B(10, 20, Add);
+	Less less;
+	Greater greater;
+
+	std::sort( // insertion sort, quick sort, heap sort 조합인 introsort로 구현돼있음
+		array.begin(),
+		array.end(),
+		less
+	);
+
+	for (const auto item : array)
+	{
+		std::cout << item << " ";
+	}
+
+	std::cout << std::endl;
 
 	std::cin.get();
 }
